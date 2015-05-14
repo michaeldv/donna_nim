134a500a64c34993ac152154ebf835bbeb5eddc5
Michael Dvorkin <mike@dvorkin.net>
2015-05-13 23:29:32 -0700

    Started with utils.nim

---
+++ src/utils.nim
@@ -0,0 +1,22 @@
+# Copyright (c) 2014-2015 by Michael Dvorkin. All Rights Reserved.
+# Use of this source code is governed by a MIT-style license that can
+# be found in the LICENSE file.
+
+proc row*(square: int): int =
+  ## Returns row number in 0..7 range for the given square.
+  return square shr 3
+
+proc col*(square: int): int =
+  ## Returns column number in 0..7 range for the given square.
+  return square and 7
+
+proc coordinate*(square: int): (int, int) =
+  ## Returns both row and column numbers for the given square.
+  return (row(square), col(square))
+
+proc square*(row, column: int): int =
+  ## Returns 0..63 square number for the given row/column coordinate.
+  return (row shl 3) + column
+
+proc c*(color: uint8): string =
+  return ["white", "black"][int(color)]
