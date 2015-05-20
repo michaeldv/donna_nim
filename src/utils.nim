2a434f7fef260b7809a303d1ca04d1bb9486d380
Michael Dvorkin <mike@dvorkin.net>
2015-05-19 21:07:47 -0700

    More utils

--- src/utils.nim
+++ src/utils.nim
@@ -2,6 +2,9 @@
 # Use of this source code is governed by a MIT-style license that can
 # be found in the LICENSE file.
 
+import unsigned
+import bitmask, data
+
 proc row*(square: int): int =
   ## Returns row number in 0..7 range for the given square.
   return square shr 3
@@ -22,6 +25,19 @@ proc rank*(color: uint8, square: int): int =
   # Returns relative rank for the square in 0..7 range. For example E2 is rank 1
   # for white and rank 6 for black.
   return row(square) xor (int(color) * 7)
+
+# Flips the square verically for white (ex. E2 becomes E7).
+proc flip*(color: uint8, square: int): int =
+  if color == White:
+    return square xor 56
+  return square
+
+proc same*(square: int): Bitmask =
+  ## Returns a bitmask with light or dark squares set matching the color of the
+  ## square.
+  if (bit[square] and maskDark) != 0:
+    return maskDark
+  return not maskDark
 
 proc c*(color: uint8): string =
   return ["white", "black"][int(color)]
